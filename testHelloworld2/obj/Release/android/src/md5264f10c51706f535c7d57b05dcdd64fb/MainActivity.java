36d2b5dd89b831ab5f3b84627a4647f7dd121785
Rob Francis <robfrancis@truckcom.co.uk>
2018-04-09 12:02:23 +0100

    Start

---
+++ testHelloworld2/obj/Release/android/src/md5264f10c51706f535c7d57b05dcdd64fb/MainActivity.java
@@ -0,0 +1,47 @@
+package md5264f10c51706f535c7d57b05dcdd64fb;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("testHelloworld2.MainActivity, testHelloworld2", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("testHelloworld2.MainActivity, testHelloworld2", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
